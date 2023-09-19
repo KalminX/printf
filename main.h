ae7702b715ed9cf491549d6fbe0ceb76b108fc0a
cosmasnwigbo <cosmasnwigbo@gmail.com>
2023-09-19 23:08:30 +0100

    update

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int putchar(char c);
+int _putchar(char c);
 int _strlen(const char *str);
 int _printf(const char *format, ...);
 void c_fmt_s(const char *s, va_list args, int *i);
